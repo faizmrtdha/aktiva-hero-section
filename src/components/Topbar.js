5268dce71f3634502257261dd85a9fddd5df2513
Faiz Murtadha Nur Wahhab <faizmrtdha99@gmail.com>
2022-02-02 15:38:49 +0700

    clear code

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Navbar, Container, Nav, FormControl } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
 
 const Topbar = () => {
